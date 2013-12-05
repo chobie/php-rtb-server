87bba09e5f3d4d5b2868c01d0caa61b362eb6ef0
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-12-06 08:00:25 +0900

    fix #2 add ip2str method. v4 address will work, but i don't test v6 address.

--- util/dump.php
+++ util/dump.php
@@ -1,6 +1,9 @@
 <?php
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . "message" . DIRECTORY_SEPARATOR . "realtime-bidding.proto.php";
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . "util" . DIRECTORY_SEPARATOR . "Util.php";
+
 $data = file_get_contents("dat");
 $request = BidRequest::parseFromString($data);
+/** @var $request BidRequest */
 
-var_dump($request);
+echo RtbUtil::ip2str($request);
